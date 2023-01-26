52a7c982078ceddf62b36d9ad62c2e485bfdae25
IrikSafin <osiris365@gmail.com>
2023-01-26 14:22:10 +0500

    добавлен функционал, просмотр сводной информации по пользователю

--- main.py
+++ main.py
@@ -12,14 +12,18 @@ import sys
 
 from connect_db import WriteDB
 from question import Question
+from select_result import SelectResult
+from utils import CLILog
 
 
 class Main:
 
     def __init__(self):
+        self.cli_log = CLILog()
         self.quest = Question()
         self.name: str = input("Введите ваше имя в формате Имя Фамилия\n")
         self.result_wr = WriteDB(self.name)
+        self.select_name = SelectResult()
         self.res_correct_answer: int = 0
         self.res_wrong_answer: int = 0
 
@@ -28,15 +32,19 @@ class Main:
         Главная функция для взаимодействия с пользователем.
         """
         while True:
-            print("1 - пройти тест")
-            print("2 - сметь пользователя")
-            print("q - выйти из программы")
+            self.cli_log.print_msg(f"\t1 - пройти тест\n"
+                                   f"\t2 - сметь пользователя\n"
+                                   f"\t3 - просмотр результатов пользователя\n"
+                                   f"\tq - выйти из программы", "gray")
+
             value = input("Выбор:\t")
             match value:
                 case "1":
                     self.res_correct_answer, self.res_wrong_answer = self.quest.io_question()
                 case "2":
                     self.relogin()
+                case "3":
+                    self.select_name.select_result(self.name)
                 case "q":
                     sys.exit(0)
             self.result_wr.write_db(correct_answer=self.res_correct_answer, wrong_answer=self.res_wrong_answer)
@@ -54,5 +62,7 @@ if __name__ == "__main__":
         main.main()
     except ValueError as ve:
         print(ve)
+    except TypeError as te:
+        print(te)
     finally:
         sys.exit(0)
